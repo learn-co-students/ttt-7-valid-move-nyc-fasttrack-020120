8c9226e8c7842148c47edb8db77b190473b675aa
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-01 20:12:16 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,15 @@
 # code your #valid_move? method here
+def valid_move?(board, i)
+  if board.size <= i
+    return false
+  end
+
+  if board[i] == 'X' || board[i] == 'O'
+    return false
+  else
+    return true
+  end
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
